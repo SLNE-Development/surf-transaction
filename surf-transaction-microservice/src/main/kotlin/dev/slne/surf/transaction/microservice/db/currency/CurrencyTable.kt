d10b52163cac1909598d02181cadd6e4309a9521
twisti-dev <76837088+twisti-dev@users.noreply.github.com>
2026-07-22 22:19:31 +0200

    ♻️ refactor(currency): change char to varchar for name and symbol fields
    - update name field type from char to varchar for better flexibility
    - update symbol field type from char to varchar for better flexibility
    🔧 chore(gradle): update Gradle version to 4.1.2

--- surf-transaction-microservice/src/main/kotlin/dev/slne/surf/transaction/microservice/db/currency/CurrencyTable.kt
+++ surf-transaction-microservice/src/main/kotlin/dev/slne/surf/transaction/microservice/db/currency/CurrencyTable.kt
@@ -6,9 +6,9 @@ import dev.slne.surf.transaction.api.currency.Currency
 import dev.slne.surf.transaction.api.currency.CurrencyScale
 
 object CurrencyTable: AuditableLongIdTable("currencies") {
-    val name = char("name", Currency.CURRENCY_NAME_MAX_LENGTH).uniqueIndex()
+    val name = varchar("name", Currency.CURRENCY_NAME_MAX_LENGTH).uniqueIndex()
     val displayName = component("display_name")
-    val symbol = char("symbol", Currency.CURRENCY_SYMBOL_MAX_LENGTH)
+    val symbol = varchar("symbol", Currency.CURRENCY_SYMBOL_MAX_LENGTH)
     val symbolDisplay = component("symbol_display")
 
     val scale = enumerationByName<CurrencyScale>("scale", 255)
